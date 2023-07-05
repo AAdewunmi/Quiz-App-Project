0ec48754fcf447f3b11b88e7e8d801f4f83df335
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-07-05 06:32:54 +0100

    Twenty-First Commit: Implement getNewQuestion() functtion

--- game.js
+++ game.js
@@ -42,4 +42,23 @@ startGame = () => {
   getNewQuestion();
 };
 
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    //go to the end page
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
 startGame();
